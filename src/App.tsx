25cea2bf545d8e89a46f3990931e9e7b0dbe45d0
maiosha99 <maimahran9899@gmail.com>
2025-10-02 00:50:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/cryptoflow" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+   <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
