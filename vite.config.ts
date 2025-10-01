249d42f6f2145abcba542aebcec80d805ea6aa04
maiosha99 <maimahran9899@gmail.com>
2025-10-02 00:43:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-    base: "/cryptoflow/",
+base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? '/cryptoflow/' : '/',
   server: {
     host: "::",
     port: 8080,
